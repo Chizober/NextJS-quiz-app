33199f454e4664f6d30a67cb18b0c948f4488e8a
Chizober <egbujie.chizoba@gmail.com>
2024-08-25 15:29:25 +0100

    first commit

---
+++ app/quiz/[1d]/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function QuizPage({ params }: { params:{id:string}} ) {
+  return (
+      <section>
+          <h1>Quiz {params.id}</h1>
+    </section>
+  )
+}
